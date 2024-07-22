03443a273d2f7c1b0dc74326fb4bce0d256d0e3d
taem1n1 <youman7953@naver.com>
2024-07-22 10:17:23 +0900

    add webflux dependecy injection

---
+++ src/main/java/com/server/ssipduck/domain/auth/presentation/KakaoOauth2Controller.java
@@ -0,0 +1,2 @@
+package com.server.ssipduck.domain.auth.presentation;public class KakaoOauth2Controller {
+}
---
+++ src/main/java/com/server/ssipduck/domain/auth/service/KakaoOauth2RegistrationService.java
@@ -0,0 +1,2 @@
+package com.server.ssipduck.domain.auth.service;public class KakaoOauth2Service {
+}
