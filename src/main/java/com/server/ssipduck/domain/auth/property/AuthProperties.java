45ac6c285295f60ed3a36f7cd41a6a5337b14371
taem1n1 <youman7953@naver.com>
2024-07-22 11:13:05 +0900

    kakao application key등 REST API 사용을 위한 프로퍼티를 불러오는 class

--- src/main/java/com/server/ssipduck/domain/auth/property/AuthProperties.java
+++ src/main/java/com/server/ssipduck/domain/auth/property/AuthProperties.java
@@ -1,4 +1,18 @@
 package com.server.ssipduck.domain.auth.property;
 
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
 public class AuthProperties {
+    @Value("${oauth2.kakao.client-id}")
+    private String clientId;
+
+    @Value("${oauth2.kakao.redirect-uri}")
+    private String redirectUri;
+
+    @Value("${oauth2.kakao.client-secret}")
+    private String clientSecret;
 }
