5ddaca5450aed4d19ad00c92cd4bfab193b21b39
taem1n1 <youman7953@naver.com>
2024-07-22 11:12:17 +0900

    kakao auth token으로 유저 프로필을 불러오는데에 성공했을때 유저 정보를 담는 dto class

--- src/main/java/com/server/ssipduck/domain/auth/dto/KakaoUserInfo.java
+++ src/main/java/com/server/ssipduck/domain/auth/dto/KakaoUserInfo.java
@@ -1,2 +1,25 @@
-package com.server.ssipduck.domain.auth.dto;public class KakaoUserInfo {
+package com.server.ssipduck.domain.auth.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor @NoArgsConstructor
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class KakaoUserInfo {
+    private Long id;
+
+    private Properties properties;
+
+    @Getter
+    @AllArgsConstructor @NoArgsConstructor
+    @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+    public static class Properties {
+        private String nickname;
+        private String profileImage;
+        private String thumbnailImage;
+    }
 }
