acca2174390091db5d9cda352892bbbed66deb75
taem1n1 <youman7953@naver.com>
2024-07-22 11:12:31 +0900

    kakao oauth2 register presentation layer

--- src/main/java/com/server/ssipduck/domain/auth/presentation/KakaoOauth2Controller.java
+++ src/main/java/com/server/ssipduck/domain/auth/presentation/KakaoOauth2Controller.java
@@ -1,2 +1,22 @@
-package com.server.ssipduck.domain.auth.presentation;public class KakaoOauth2Controller {
+package com.server.ssipduck.domain.auth.presentation;
+
+import com.server.ssipduck.domain.auth.dto.AuthTokenResponse;
+import com.server.ssipduck.domain.auth.service.KakaoOauth2RegistrationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class KakaoOauth2Controller {
+
+    private final KakaoOauth2RegistrationService kakaoOauth2RegistrationService;
+
+    @RequestMapping(method = RequestMethod.GET, value = "v1/oauth/kakao")
+    private ResponseEntity<?> registerByKakao(@RequestParam String code) {
+        return ResponseEntity.ok(kakaoOauth2RegistrationService.registrationKakaoUser(code));
+    }
 }
