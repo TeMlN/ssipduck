3021a0c1bb8e5aba657cb377c8bcc96d9d3232fe
taem1n1 <youman7953@naver.com>
2024-07-22 09:36:23 +0900

    initial commit

---
+++ src/main/java/com/server/ssipduck/SsipduckApplication.java
@@ -0,0 +1,13 @@
+package com.server.ssipduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsipduckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsipduckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/ssipduck/SsipduckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.ssipduck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsipduckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
