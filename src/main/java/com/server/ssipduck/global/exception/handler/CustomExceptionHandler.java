58df49ba4de48b19cc2491e75c14774d3dba1160
taem1n1 <youman7953@naver.com>
2024-10-13 06:36:16 +0900

    ADD Custom Exception Classes

--- src/main/java/com/server/ssipduck/global/exception/handler/CustomExceptionHandler.java
+++ src/main/java/com/server/ssipduck/global/exception/handler/CustomExceptionHandler.java
@@ -1,2 +1,35 @@
-package com.server.ssipduck.global.exception.handler;public class CustomExceptionHandler {
+package com.server.ssipduck.global.exception.handler;
+
+import com.server.ssipduck.global.exception.CustomException;
+import com.server.ssipduck.global.exception.response.ExceptionResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    private ResponseEntity<ExceptionResponseEntity> customExceptionHandler(CustomException e) {
+        return ResponseEntity.status(e.getExceptionCode().getHttpStatus())
+                .body(ExceptionResponseEntity.of(e.getExceptionCode()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> dtoValidationExceptionHandler(final MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach((error)-> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errors);
+    }
 }
--- src/main/java/com/server/ssipduck/global/exception/response/ExceptionResponseEntity.java
+++ src/main/java/com/server/ssipduck/global/exception/response/ExceptionResponseEntity.java
@@ -1,2 +1,19 @@
-package com.server.ssipduck.global.exception.response;public class ExceptionResponseEntity {
+package com.server.ssipduck.global.exception.response;
+
+import com.server.ssipduck.global.exception.ExceptionCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ExceptionResponseEntity {
+
+    private final String code;
+    private String message;
+    private Boolean success;
+
+    public static ExceptionResponseEntity of(ExceptionCode exceptionCode) {
+        return new ExceptionResponseEntity(exceptionCode.getCode(), exceptionCode.getMessage(), exceptionCode.getSuccess());
+    }
+
 }
