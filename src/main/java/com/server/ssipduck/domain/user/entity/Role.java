c085ea763620df1cf14f0042baae94c022d6745c
taem1n1 <youman7953@naver.com>
2024-10-13 06:31:58 +0900

    ADD Role

--- src/main/java/com/server/ssipduck/domain/user/entity/Role.java
+++ src/main/java/com/server/ssipduck/domain/user/entity/Role.java
@@ -1,2 +1,12 @@
-package com.server.ssipduck.domain.user.entity;public enum Role {
+package com.server.ssipduck.domain.user.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    ROLE_PERSONAL;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
