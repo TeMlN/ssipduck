6ff53447085c3a58d8273ec7dadb81bac0068f2e
taem1n1 <youman7953@naver.com>
2024-10-13 06:37:08 +0900

    ADD Custom UserDetailsService

--- src/main/java/com/server/ssipduck/global/security/custom/CustomUserDetailsService.java
+++ src/main/java/com/server/ssipduck/global/security/custom/CustomUserDetailsService.java
@@ -1,2 +1,21 @@
-package com.server.ssipduck.global.security.custom;public class CustomUserDetailsService {
+package com.server.ssipduck.global.security.custom;
+
+import com.server.ssipduck.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByAuthId(Long.valueOf(username))
+                .orElseThrow(() -> new IllegalArgumentException("asd"));
+    }
 }
