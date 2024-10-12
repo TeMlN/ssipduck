38f8b646cca2689b8a2eab18d5474421cc412941
taem1n1 <youman7953@naver.com>
2024-10-13 06:30:30 +0900

    ADD Custom JWT Security Filter

--- src/main/java/com/server/ssipduck/global/security/filter/TokenFilter.java
+++ src/main/java/com/server/ssipduck/global/security/filter/TokenFilter.java
@@ -1,2 +1,40 @@
-package com.server.ssipduck.global.security.filter;public class TokenFilter {
+package com.server.ssipduck.global.security.filter;
+
+import com.server.ssipduck.global.security.helper.TokenHelper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class TokenFilter extends OncePerRequestFilter {
+
+    private final TokenHelper tokenHelper;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String verifiedToken = resolveToken(request);
+        if (StringUtils.hasText(verifiedToken) && tokenHelper.validateToken(verifiedToken)) {
+            Authentication authentication = tokenHelper.getAuthentication(verifiedToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        if (StringUtils.hasText(token) && token.startsWith("Bearer "))
+            return token.substring(7);
+        return token;
+    }
 }
--- src/main/java/com/server/ssipduck/global/security/filter/TokenFilterConfiguration.java
+++ src/main/java/com/server/ssipduck/global/security/filter/TokenFilterConfiguration.java
@@ -1,2 +1,20 @@
-package com.server.ssipduck.global.security.filter;public class TokenFilterConfiguration {
+package com.server.ssipduck.global.security.filter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class TokenFilterConfiguration extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final TokenFilter tokenFilter;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
 }
