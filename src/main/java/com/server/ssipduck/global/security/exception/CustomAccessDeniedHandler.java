421942784000df77c853a29f2472d847de63ddfa
taem1n1 <youman7953@naver.com>
2024-10-13 06:36:30 +0900

    ADD JWT Exception

--- src/main/java/com/server/ssipduck/global/security/exception/CustomAccessDeniedHandler.java
+++ src/main/java/com/server/ssipduck/global/security/exception/CustomAccessDeniedHandler.java
@@ -1,2 +1,18 @@
-package com.server.ssipduck.global.security.exception;public class CustomAccessDeniedHandler {
+package com.server.ssipduck.global.security.exception;
+
+import java.io.IOException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+    }
+
 }
