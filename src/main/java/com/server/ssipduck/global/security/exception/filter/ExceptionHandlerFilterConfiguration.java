24573c661c14834ea4a96d72ad6b590e1fab550c
taem1n1 <youman7953@naver.com>
2024-10-13 06:31:46 +0900

    ADD Custom JWT Exception Filter

--- src/main/java/com/server/ssipduck/global/security/exception/filter/ExceptionHandlerFilterConfiguration.java
+++ src/main/java/com/server/ssipduck/global/security/exception/filter/ExceptionHandlerFilterConfiguration.java
@@ -1,2 +1,20 @@
-package com.server.ssipduck.global.security.exception.filter;public class ExceptionHandlerFilterConfiguration {
+package com.server.ssipduck.global.security.exception.filter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class ExceptionHandlerFilterConfiguration extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final ExceptionHandlerFilter exceptionHandlerFilter;
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.addFilterBefore(exceptionHandlerFilter, SecurityContextPersistenceFilter.class);
+    }
 }
